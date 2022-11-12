9ee5e83bc1637d657b38b4f426d2b971d1360917
ThilinaJs <112922490+ThilinaJs@users.noreply.github.com>
2022-11-12 13:39:36 +0530

    Update PythonCode.py

--- PythonCode.py
+++ PythonCode.py
@@ -23,5 +23,18 @@ search.send_keys('Kitchen Appliance')
 
 search.send_keys(Keys.ENTER)
 
-product_name =driver.find_elements("xpath", '//li[@class="searchResultItem"]/div/div[2]/a')
+product_name =driver.find_elements("xpath", '//li[@class="searchResultItem"]/div/div[2]/a[1]')
+product_new_price = driver.find_elements("xpath", '//li[@class="searchResultItem"]/div/div[3]')
+product_old_price = driver.find_elements("xpath", '//li[@class="searchResultItem"]/div/div[3]/span')
+likes = driver.find_elements("xpath", '//li[@class="searchResultItem"]/div/div[5]/div/span')
 
+w=[]
+for i in product_new_price:
+    w.append(i.text[0:3])
+
+df = pd.DataFrame(columns=['Product Name','Product New Price','Product Old Price','No of Likes'])
+
+for i in range(len(product_name)):
+    df = df.append({'Product Name':product_name[i].text,'Product New Price':w[i],'Product Old Price':product_old_price[i].text,'No of Likes':likes[i].text}, ignore_index=True)
+
+df.to_excel('KitchenApplianceDetails.xlsx',index=False)
